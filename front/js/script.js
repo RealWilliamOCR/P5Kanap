2b6c274cda2e4afb62b00ee8545a1b8b54c64734
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-02-08 15:20:57 +0100

    Update manuel du fichier script.js
    Apparition des div et forEach mis en place afin de dupliquer autant de div qu'il y a d'articles.

--- front/js/script.js
+++ front/js/script.js
@@ -1,34 +1,27 @@
-const items = document.getElementById("items");
-const h3 = document.getElementsByClassName("productName");
-const p = document.getElementsByClassName("productDescription");
+fetch('http://localhost:3000/api/products')
+.then(function(res){
+    if(res.ok){
+        return res.json();
+    }
+    console.log(res)
+    })
+    .then(function (datas){
+        datas.forEach(products => {
 
-fetch("http://localhost:3000/api/products")
-	.then(function(res){
-		if(res.ok){
-			return res.json();
-		}
-		console.log(res);
-	})
-	
-let lien = document.createElement("a");
-lien.setAttribute("href", "http://localhost:3000/api/products")
-document.getElementById("items").appendChild("lien");
+        let items = document.getElementById('items');
+        a = document.createElement('a');
+        article = document.createElement('article');
+        img = document.createElement('img');
+        img.classList.add("productImage")
+        h3 = document.createElement('h3');
+            h3.classList.add("productName");
+        p = document.createElement('p');
+            p.classList.add("productDescription");
 
-let article = document.createElement("article");
-items.appendChild("article");
-
-let image = document.createElement("img");
-image.setAttribute("src", "../back/images/kanap01.jpeg")
-article.appendChild("image");
-
-let image = document.createElement("img");
-image.setAttribute("alt")
-article.appendChild("image");
-
-let titre = document.createElement("h3");
-titre.setAttribute("class", "productName");
-article.appendChild("h3");
-
-let texte = document.createElement("p");
-texte.setAttribute("class", "productDescription");
-article.appendChild("p");
+        items.appendChild(a);
+        a.appendChild(article);
+        article.appendChild(img);
+        article.appendChild(h3);
+        article.appendChild(p);
+    });
+});
