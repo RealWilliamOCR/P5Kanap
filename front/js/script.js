8b8cab1bb7877f38fa8751a1854a949bef9095c6
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-02-14 12:28:58 +0100

    Update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -3,20 +3,24 @@ fetch('http://localhost:3000/api/products')
     if(res.ok){
         return res.json();
     }
-    console.log(res)
     })
     .then(function (datas){
         datas.forEach(products => {
 
         let items = document.getElementById('items');
         a = document.createElement('a');
+            a.href="./product.html?id=$";
         article = document.createElement('article');
         img = document.createElement('img');
         img.classList.add("productImage")
+            img.src = products.imageUrl;
+            img.alt = products.altTxt;
         h3 = document.createElement('h3');
             h3.classList.add("productName");
+            h3.innerHTML = products.name;
         p = document.createElement('p');
             p.classList.add("productDescription");
+            p.innerHTML = products.description;
 
         items.appendChild(a);
         a.appendChild(article);
