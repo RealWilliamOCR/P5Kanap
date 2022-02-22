8dedb9604f2dcad4232119eedb0797da0d9a366b
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-02-22 14:34:21 +0100

    Commit
    Cette fois-ci les infos sont bien celles du produit concerné, le Kanap Sinopé.

--- front/js/product.js
+++ front/js/product.js
@@ -8,12 +8,17 @@ fetch('http://localhost:3000/api/products?id=107fb5b75607497b96722bda5b504926')
         datas.forEach(products => {
 
         let items = document.getElementById('title');
-                items.innerHTML = products.name;
+            items.innerHTML = products.name;
 
-        /*let items = document.getElementsByClassName('item__img');
-        let img = document.createElement('img');
-        img.classList.add("productImage")
+        let price = document.getElementById('price');
+            price.innerHTML = products.price;
+
+        let description = document.getElementById('description');
+            description.innerHTML = products.description;
+
+        let img = document.getElementsByClassName('item__img');
+            img.classList.add("productImage");
             img.src = ("datas.imageUrl");
-            img.alt = ("Photographie d'un canapé");*/
+            img.alt = ("Photographie d'un canapé");
     });
 });
