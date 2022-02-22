13cbc65ad56826a57ee0c7ced5dc23dfa9c93901
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-02-22 13:10:46 +0100

    Commit
    Ajout d'un titre h1 visible.

--- front/js/product.js
+++ front/js/product.js
@@ -8,8 +8,7 @@ fetch('http://localhost:3000/api/products?id=107fb5b75607497b96722bda5b504926')
         datas.forEach(products => {
 
         let items = document.getElementById('title');
-        h1 = document.createElement('h1');
-            h1.innerHTML = products.name;
+                items.innerHTML = products.name;
 
         /*let items = document.getElementsByClassName('item__img');
         let img = document.createElement('img');
@@ -18,7 +17,3 @@ fetch('http://localhost:3000/api/products?id=107fb5b75607497b96722bda5b504926')
             img.alt = ("Photographie d'un canapé");*/
     });
 });
-
-        /*if ($_id = "?id=107fb5b75607497b96722bda5b504926") {
-            console.log ("colors", "name", "price", "imageUrl", "description", "altTxt")
-        }*/
