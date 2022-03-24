037ed231bc2c849030964c1d6f4c6248c4d71ff8
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-03-24 07:34:32 +0100

    Commit
    Ajout de toutes les informations nécessaire du canapé dans la page cart.
    Ajout de la suppression du localStorage et du canapé avec ses infos dans la page cart.

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,13 +4,7 @@ fetch('http://localhost:3000/api/products')
         return res.json();
     }
     })
-
-    /*localStorage.getItem("Couleurs", "product-color");
-    localStorage.getItem("Image", "image");
-    localStorage.getItem("Description", "description");*/
-
-    /*.then(function (products){*/
-
+    
        /* function test() {*/
             let section = document.getElementById("cart__items");
 
@@ -22,51 +16,83 @@ fetch('http://localhost:3000/api/products')
 
             let image = document.createElement("img");
             image.src = localStorage.getItem("Image");
+            image.alt = localStorage.getItem("ImageAlt");
+
+            let itemContent = document.createElement("div");
+            itemContent.classList.add("cart__item__content");
+
+            let itemDesc = document.createElement("div");
+            itemDesc.classList.add("cart__item__content__description");
 
             let h2 = document.createElement("h2");
             h2.classList.add("productName");
             h2.innerHTML = localStorage.getItem("Nom");
 
-            let desc = document.createElement("p");
+            let color = document.createElement("p");
+            color.innerHTML = localStorage.getItem("Couleurs");
+
+            let price = document.createElement("p");
+            price.innerHTML = localStorage.getItem("Prix")+(' €');
+
+            let itemSettings = document.createElement("div");
+            itemSettings.classList.add("cart__item__content__settings");
+
+            let itemSetQuantity = document.createElement("div");
+            itemSetQuantity.classList.add("cart__item__content__settings__quantity");
+
+            let nomQte = document.createElement("p");
+            nomQte.innerHTML = ('Qté : ')+localStorage.getItem("Quantite");
+
+            let itemDelete = document.createElement("div");
+            itemDelete.classList.add("cart__item__content__settings__delete");
+
+            let deleteAccept = document.createElement("p");
+            deleteAccept.innerHTML = ('Supprimer');
 
             section.appendChild(article);
             article.appendChild(itemImg);
+            article.appendChild(itemContent);
             itemImg.appendChild(image);
-            article.appendChild(h2);
-    /*}*/
-    /*});*/
+            itemContent.appendChild(itemDesc);
+            itemDesc.appendChild(h2);
+            itemDesc.appendChild(color);
+            itemDesc.appendChild(price);
+            article.appendChild(itemSettings);
+            itemSettings.appendChild(itemSetQuantity);
+            itemSetQuantity.appendChild(nomQte);
+            article.appendChild(itemDelete);
+            itemDelete.appendChild(deleteAccept);
 
-    /* BIEN PENSER A RAJOUTER LE ALT */
+            itemDelete.addEventListener("click", function (){
 
-    /*.then(function (datas){
-        datas.forEach(products => {
+                localStorage.removeItem("Couleurs");
+                localStorage.removeItem("Quantite");
+                localStorage.removeItem("Image");
+                localStorage.removeItem("ImageAlt");
+                localStorage.removeItem("Description");
+                localStorage.removeItem("Nom");
+                localStorage.removeItem("Prix");
 
-        let article = document.getElementsByClassName("cart__item");
-            article = document.createElement("product-ID");
-            article = document.createElement("product-color");
+                section.remove("itemcart__item__content__settings__delete");
+            });
 
-        let articleimg = document.getElementsByClassName("cart__item__img");
-            articleimg = document.createElement("img");
-            img.classList.add("productImage");
-            img.src = products.imageUrl;
-            img.alt = products.altTxt;
+            function getTotals(){
 
-        let articledesc = document.getElementsByClassName("cart__item__content");
-            articledesc = document.createElement("cart__item__content__description");
+                let articleQte = document.getElementsByClassName("itemQuantity");
+                let articleQteLength = articleQte.length;
+                quantityTotal = 0;
+                for (let i = 0; i < articleQteLength; ++i) {
 
-        let h2 = document.createElement("h2");
-            h2.classList.add("productName");
-            h2.innerHTML = products.name;
+                    quantityTotal += articleQte[i].valueAsNumber;
+                }
+                let productCartTotalQuantity = document.getElementById("totalQuantity");
+                productCartTotalQuantity.innerText = quantityTotal;
 
-        let p = document.createElement("p");
-            p.classList.add("productColor");
-            p.innerHTML = products.colors;
-            p.classList.add("productPrice");
-            p.innerHTML = products.price;
-        
-        article.appendChild(articleimg);
-        article.appendChild(articledesc);
-        article.appendChild(h2);
-        article.appendChild(p);
-        });
-    });*/
+                totalPrice = 0;
+                for (let i= 0; i < articleQteLength; ++i) {
+                    totalPrice += articleQte[i].valueAsNumber * produitLocalStorage[i].price;
+                }
+                let productCartTotalPrice = document.getElementById("totalPrice");
+                productCartTotalPrice.innerText = totalPrice;
+            }
+            getTotals();
